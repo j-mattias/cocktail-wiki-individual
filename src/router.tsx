3f8de45f556120a4757df7490f1d17989039a14d
Mattias <mattiasjohansson.vfx@gmail.com>
2024-09-11 12:02:08 +0200

    Add randomCocktailLoader to fetch random cocktail

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import { HomePage, SearchPage } from "./pages";
 import { RootLayout } from "./layouts";
+import { randomCocktailLoader } from "./loaders";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />} path="/">
-      <Route element={<HomePage />} index />
+      <Route element={<HomePage />} loader={randomCocktailLoader} index />
       <Route element={<SearchPage />} path="search" />
     </Route>
   )
